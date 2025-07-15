2f68988bde9249db62af281989ef4a1704af7d43
alvaromingue <115833530+alvaromingue@users.noreply.github.com>
2025-07-15 18:14:23 +0200

    Update work.js

--- scripts/work.js
+++ scripts/work.js
@@ -9,15 +9,12 @@ const projectList = [
 ];
 
 const gallery = document.querySelector('.gallery');
-projectList.forEach(proj => {
-  fetch(`projects/${proj}/info.json`).then(res => res.json()).then(info => {
-    gallery.innerHTML += `
-      <div class="gallery-item" onclick="window.location.href='project.html?id=${proj}'">
-        <img src="projects/${proj}/thumb.jpg" alt="${info.title}">
-        <div class="gallery-info">
-          <div class="project-name">${info.title}</div>
-          <div class="project-type">${info.tag}</div>
-        </div>
-      </div>`;
-  });
-});
+gallery.innerHTML = projectData.map(project => `
+  <div class="gallery-item" onclick="window.location.href='project.html?id=${project.id}'">
+    <img src="${project.thumb}" alt="${project.title}">
+    <div class="gallery-info">
+      <div class="project-name">${project.title}</div>
+      <div class="project-type">${project.tag}</div>
+    </div>
+  </div>
+`).join('');
